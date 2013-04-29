a1924d4a073c9fd406f7d6bbe555b598333cd9d0
Paul <paulmerlyn@sbcglobal.net>
2013-04-29 16:46:37 -0700

    Update databaseconnector.php

--- ssi/databaseconnector.php
+++ ssi/databaseconnector.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_HOST', 'localhost'); //host of mysql
-define('DB_USER', 'paulme6_merlyn'); //username of the mysql user
-define('DB_PASS', 'fePhaCj64mkik'); //password for mysql user
-define('DB_DATABASE', 'paulme6_nerdwallet'); //mysql database to connect to
+define('DB_HOST', '****'); //host of mysql
+define('DB_USER', '****'); //username of the mysql user
+define('DB_PASS', '****'); //password for mysql user
+define('DB_DATABASE', '****'); //mysql database to connect to
 
 $db = new mysqli ( DB_HOST, DB_USER, DB_PASS, DB_DATABASE );
 
